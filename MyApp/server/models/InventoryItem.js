1b17b6f63cdf857f6ff33c6098cfbc1394a95ff8
JmRaine <justinraine0514@gmail.com>
2026-03-29 00:38:54 +0800

    new model

--- MyApp/server/models/InventoryItem.js
+++ MyApp/server/models/InventoryItem.js
@@ -1,21 +1,107 @@
 const mongoose = require('mongoose');
 
-const inventoryItemSchema = new mongoose.Schema({
-  type: { type: String, enum: ['goods', 'monetary'], default: 'goods' },
-  name: String,
-  category: String,
-  quantity: {
-    type: Number,
-    default: 0
+const inventoryItemSchema = new mongoose.Schema(
+  {
+    type: {
+      type: String,
+      enum: ['goods', 'monetary'],
+      required: true,
+      default: 'goods',
+      trim: true
+    },
+
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    category: {
+      type: String,
+      enum: ['food', 'clothing', 'hygiene'],
+      required: function () {
+        return this.type === 'goods';
+      },
+      default: undefined
+    },
+
+    quantity: {
+      type: Number,
+      required: function () {
+        return this.type === 'goods';
+      },
+      min: 0,
+      default: undefined
+    },
+
+    unit: {
+      type: String,
+      required: function () {
+        return this.type === 'goods';
+      },
+      trim: true,
+      default: undefined
+    },
+
+    amount: {
+      type: Number,
+      required: function () {
+        return this.type === 'monetary';
+      },
+      min: 0,
+      default: undefined
+    },
+
+    description: {
+      type: String,
+      trim: true,
+      default: ''
+    },
+
+    sourceType: {
+      type: String,
+      enum: ['external', 'government', 'internal'],
+      default: 'external',
+      trim: true
+    },
+
+    sourceName: {
+      type: String,
+      trim: true,
+      default: ''
+    },
+
+    proofFiles: {
+      type: [String],
+      default: []
+    },
+
+    addedBy: {
+      type: String,
+      trim: true,
+      default: ''
+    },
+
+    isArchive: {
+      type: Boolean,
+      default: false
+    }
   },
-  unit: String,
-  description: String,
-  proofFiles: [String],
-  addedBy: String,
-  isArchive: {
-    type: Boolean,
-    default: false
+  { timestamps: true }
+);
+
+inventoryItemSchema.pre('validate', function (next) {
+  if (this.type === 'goods') {
+    this.amount = undefined;
   }
-}, { timestamps: true });
+
+  if (this.type === 'monetary') {
+    this.category = undefined;
+    this.quantity = undefined;
+    this.unit = undefined;
+  }
+
+  next();
+});
 
 module.exports = mongoose.model('InventoryItem', inventoryItemSchema);
