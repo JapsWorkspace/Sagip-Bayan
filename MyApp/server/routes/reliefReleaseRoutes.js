8bef0b5c86838aaac0ee1d5d753c1ace9ed73193
JmRaine <justinraine0514@gmail.com>
2026-03-29 17:37:07 +0800

    Add files via upload

---
+++ MyApp/server/routes/reliefReleaseRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/ReliefReleaseController');
+
+router.get('/approved-requests', controller.getApprovedRequestsForRelease);
+router.post('/', controller.createReliefRelease);
+router.get('/', controller.getAllReliefReleases);
+router.get('/request/:reliefRequestId', controller.getReleasesByRequest);
+
+module.exports = router;
