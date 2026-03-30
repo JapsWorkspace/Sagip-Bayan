38c6eb26ffdb7492e6bc35fa6a31149b110a9ce2
JmRaine <justinraine0514@gmail.com>
2026-03-31 01:41:31 +0800

    Add enum for barangayName and unique index

--- MyApp/server/models/Barangay.js
+++ MyApp/server/models/Barangay.js
@@ -1,12 +1,42 @@
 const mongoose = require('mongoose');
 
+const BARANGAY_OPTIONS = [
+  "Bagong Sikat",
+  "Bagong Silang",
+  "Calabasa",
+  "Don Mariano Marcos",
+  "Dampulan",
+  "Hilera",
+  "Imelda Poblacion",
+  "Ibunia",
+  "Lambakin",
+  "Langla",
+  "Magsalisi",
+  "Malabon Kaingin",
+  "Marawa",
+  "Niyugan",
+  "Putlod",
+  "San Jose",
+  "San Pablo",
+  "San Roque",
+  "Santo Tomas Norte",
+  "Santo Tomas Sur",
+  "Sapang Putik",
+  "Ulanin-Pitak"
+];
+
 const barangaySchema = new mongoose.Schema(
   {
     username: { type: String, required: true, trim: true },
     email: { type: String, unique: true, required: true, trim: true, lowercase: true },
     password: { type: String, required: true },
 
-    barangayName: { type: String, required: true, trim: true },
+    barangayName: {
+      type: String,
+      required: true,
+      trim: true,
+      enum: BARANGAY_OPTIONS
+    },
 
     verified: { type: Boolean, default: true },
 
@@ -18,6 +48,14 @@ const barangaySchema = new mongoose.Schema(
     archivedAt: { type: Date, default: null }
   },
   { timestamps: true }
+);
+
+barangaySchema.index(
+  { barangayName: 1 },
+  {
+    unique: true,
+    partialFilterExpression: { archived: false }
+  }
 );
 
 module.exports = mongoose.model('Barangay', barangaySchema);
